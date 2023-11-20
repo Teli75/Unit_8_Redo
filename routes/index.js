ceb1d0161497ff630f57367934e95d668e43a683
Teli <7telita@gmail.com>
2023-11-20 12:46:15 -0800

    books do not display

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+const Book = require("../models/book.js");
+
+
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  try {
+    const allBooks = await Book.findAll();
+    console.log(allBooks.map(book => book.toJSON()));
+    res.json(allBooks);
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    res.status(500).send("server error");
+  }
+});
+
+module.exports = router;
