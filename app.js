a260748cb5d472e4bffbac471a8839c533886d0a
Teli <7telita@gmail.com>
2023-11-20 20:31:03 -0800

    book and book/new views

--- app.js
+++ app.js
@@ -1,44 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const { sequelize } = require('./models/index'); 
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const { sequelize } = require("./models/index");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 (async () => {
   //await sequelize.sync({ force: true });
@@ -49,4 +32,57 @@ app.use(function(err, req, res, next) {
     console.error("Error connecting to the database: ", error);
   }
 })();
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   console.log("I'm sorry, but this page is not found");
+//   next(createError(404));
+// });
+// app.get("/server-error", (req, res, next) => {
+//   console.log("Simulated server error");
+//   const err = new Error("Simulated server error");
+//   err.status = 500;
+//   next(err);
+// });
+
+app.use((req, res, next) => {
+//app.get("/test-404", (req, res, next) => {
+  console.log("I'm sorry, but this page is not found");
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+
+app.use((err, req, res, next) => {
+  //set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  
+  if (err.status === 404) {
+    res.status(404);
+    err.status = 404;
+    err.message = "I'm sorry, but this page isn't found";
+    res.render("page-not-found", { err });
+  } else {
+    err.status = err.status || 500;
+    console.log(err.status);
+    err.message = err.message || "Whoops!";
+
+    res.status(err.status);
+    res.render("error", { err });
+  }
+});
 module.exports = app;
+
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,38 @@ const { Book } = require("../models/index.js");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
+  console.log("Handling GET request for /");
   try {
     const allBooks = await Book.findAll();
     console.log(allBooks.map(book => book.toJSON()));
     res.json(allBooks);
   } catch (error) {
-    console.error("Error fetching books:", error);
+    console.error("Error getting books:", error);
     res.status(500).send("server error");
   }
 });
 
+router.get("/books", async function (req, res, next) {
+  console.log("handling GET req for /books");
+  try {
+    const allBooks = await Book.findAll();
+    console.log(allBooks.map((book) => book.toJSON()));
+    res.render('layout', { allBooks });
+  } catch (error) {
+    console.error("error getting books:", error);
+    res.status(500).send("server error");
+  }
+});
+
+router.get("/books/new", async function (req, res, next) {
+  console.log("handling GET req for /books/new");
+  try {
+    const allBooks = await Book.findAll();
+    console.log(allBooks.map((book) => book.toJSON()));
+    res.render('new-book', { allBooks });
+  } catch (error) {
+    console.error("error fetching books:", error);
+    res.status(500).send("server error");
+  }
+});
 module.exports = router;
